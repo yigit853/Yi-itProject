ea5ce6839a70f55f8b4aaba5250e3ef34ce8033a
yigitatci <yigitatci@bitbucket.org>
2024-06-03 14:40:01 +0300

    second

--- YigitProject/Controllers/VoucherController.cs
+++ YigitProject/Controllers/VoucherController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 using YigitProject.Models;
-
+using Newtonsoft.Json;
 namespace YigitProject.Controllers
 {
     [Route("api/[controller]")]
@@ -8,9 +9,15 @@ namespace YigitProject.Controllers
     public class VoucherController : Controller
     {
         [HttpPost]
-        public IActionResult Voucher(List<VoucherModel> voucher)
-        {
-            return Json(voucher);
+        public IActionResult Voucher(List<VoucherModel> vouchers)
+        {         
+            foreach (var voucher in vouchers)
+            {
+                Console.WriteLine($"Metin: {voucher.Description}");
+                Console.WriteLine($"Koordinatlar: x={voucher.boundingPoly.Vertices[0].X}, y={voucher.boundingPoly.Vertices[0].Y}");
+                Console.WriteLine();
+            }
+            return Json(vouchers);
         }
     }
 }
